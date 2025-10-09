5bd05bbeaa7fda8631acda388d3ee70d01320fbe
Dobrivoje Vujovic <dobrivojevujovic321@gmail.com>
2025-10-09 20:11:15 +0200

    Initialized app.

---
+++ app/src/MyApp.cpp
@@ -0,0 +1,14 @@
+//
+// Created by radisa on 9.10.25..
+//
+
+#include "MyApp.hpp"
+
+#include <spdlog/spdlog.h>
+
+namespace app {
+    void MyApp::app_setup() {
+        spdlog::info("App setup");
+
+    }
+} // app
