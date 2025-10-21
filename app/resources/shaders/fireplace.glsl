c1d0f8c9a217aa40513a4eb40bd99b71755a8909
Dobrivoje Vujovic <dobrivojevujovic321@gmail.com>
2025-10-21 23:56:44 +0200

    Added separate shader for each model.

--- app/resources/shaders/fireplace.glsl
+++ app/resources/shaders/fireplace.glsl
@@ -5,14 +5,11 @@ layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aNormal;
 layout (location = 2) in vec2 aTexCoords;
 
-
 out vec2 TexCoords;
-out vec3 FragPos;
 
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
-
 
 void main() {
     TexCoords = aTexCoords;
@@ -26,7 +23,12 @@ in vec2 TexCoords;
 out vec4 FragColor;
 
 uniform sampler2D texture_diffuse1;
+uniform float time;
 
 void main() {
-    FragColor = vec4(vec3(texture(texture_diffuse1, TexCoords)), 1.0f);
+    vec3 fireColor = vec3(texture(texture_diffuse1, TexCoords));
+    float flicker = 0.8 + 0.2 * (sin(time * 6.0) * sin(time * 1.7) * sin(time * 2.1));
+    vec3 emissive = fireColor * flicker;
+
+    FragColor = vec4(emissive, 1.0f);
 }
